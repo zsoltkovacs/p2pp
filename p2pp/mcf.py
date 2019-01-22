b80b737becda16eb3aa1263cc985013ad2241065
tomvandeneede <t.vandeneede@pandora.be>
2019-01-22 05:27:34 +0100

    Added parameter ;P2PP LINEARPINGLENGTH= ONLY WORKS when LINEARPING is set

--- p2pp/mcf.py
+++ p2pp/mcf.py
@@ -529,8 +529,7 @@ def gcode_parseline(splice_offset, gcode_fullline):
         v.pingLengthMultiplier = 1.0
 
     if gcode_fullline.startswith(";P2PP LINEARPINGLENGTH="):
-        if v.pingLengthMultiplier == 1.0
-            v.pingIntervalLength = float(gcode_fullline[23:])
+        v.pingIntervalLength = float(gcode_fullline[23:])
 
     if gcode_fullline.startswith(";P2PP SIDEWIPEMINY="):
         v.sideWipeMinY=float(gcode_fullline[19:])
