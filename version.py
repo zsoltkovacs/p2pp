00a4d651cfdf70297fbf6610519463f90270ac6c
tomvandeneede <t.vandeneede@pandora.be>
2019-07-16 21:33:32 +0200

    Updated version to 3.2.1 for test

--- version.py
+++ version.py
@@ -11,7 +11,7 @@ __status__ = 'BETA'
 # general version info
 MajorVersion = 3
 MinorVersion = 2
-Build        = 0
+Build        = 1
 Train = 'Master'
 Version = "{Major}.{Minor}.{Build}-{Train}"\
     .format(Major=MajorVersion, Minor=MinorVersion, Build=Build, Train=Train)
