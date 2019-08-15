704203bcf5d42928cb5e55991868747bd4d36fc2
tomvandeneede <t.vandeneede@pandora.be>
2019-08-15 14:56:40 +0200

    Version check with online repository

--- version.py
+++ version.py
@@ -8,10 +8,39 @@ __maintainer__ = 'Tom Van den Eede'
 __email__ = 'P2PP@pandora.be'
 __status__ = 'BETA'
 
+
+import urllib
+
+
 # general version info
 MajorVersion = 3
 MinorVersion = 3
 Build = 3
 
-Version = "{Major}.{Minor}.{Build}" \
-    .format(Major=MajorVersion, Minor=MinorVersion, Build=Build)
+latest_stable_version = ""
+
+try:
+    latestversionpy = urllib.urlopen("https://github.com/tomvandeneede/p2pp/raw/master/version.py")
+    versioncontents = "".join(latestversionpy.read()).split('\n')
+    _maj=0
+    _min=0
+    _bld=0
+
+    for line in versioncontents:
+        if line.startswith("MajorVersion"):
+            _maj = int(line[-2:])
+        if line.startswith("MinorVersion"):
+            _min = int(line[-2:])
+        if line.startswith("Build"):
+            _bld = int(line[-2:])
+
+    latest_stable_version = "{}.{}.{}".format(_maj, _min, _bld)
+
+
+except IOError:
+    pass
+
+Version = "{}.{}.{}".format(MajorVersion, MinorVersion, Build)
+
+if len(latest_stable_version)>0:
+    Version = Version + "  (Latest Stable Version: " + latest_stable_version +")"
