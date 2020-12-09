1ccb175eeb867995924d974f0eccf0a82cf0bd11
tomvandeneede <t.vandeneede@pandora.be>
2020-12-09 07:40:17 +0100

    --

--- p2pp/purgetower.py
+++ p2pp/purgetower.py
@@ -294,9 +294,8 @@ def purge_generate_sequence():
     if v.manual_filament_swap:
         gcode.issue_code("G91")
         gcode.issue_code("G1 Z20 F10800")
-        gcode.issue_code("M25")
-        gcode.issue_code("G1 Z-20 F10800")
         gcode.issue_code("G90")
+        gcode.issue_code("M25")
 
     gcode.issue_code("G1 Z{:.2f} F10800".format((v.purgelayer + 1) * v.layer_height))
 
